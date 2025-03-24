b0c49d51c26619e97a24305f23029b0c6c8602af
Wikcio1618 <ciolekwiktor@gmail.com>
2025-03-24 22:56:10 +0100

    RWR and SRW implemented

--- Reconstruction.jl
+++ Reconstruction.jl
@@ -1,6 +1,7 @@
 using Graphs
 using DataStructures
-
+using LinearAlgebra
+using SparseArrays
 
 """
 Reconstructs the graph based on similarity score specified by `score_type`. 
@@ -30,10 +31,10 @@ function get_RA_scores(g::SimpleGraph)::Dict{Tuple{Int,Int},Float64}
         neis = neighbors(g, u)
         for i in 1:length(neis)-1
             for j in i+1:length(neis)
-                v, w = neis[i], neis[j]
-                pair = (min(v, w), max(v, w))
+                x, y = neis[i], neis[j]
+                pair = (min(x, y), max(x, y))
                 if !haskey(scores, pair)
-                    ra_score = sum(x -> length(x) == 0 ? 0 : 1 / degree(g, x), intersect(neighbors(g, v), neighbors(g, w)))
+                    ra_score = sum(x -> length(x) == 0 ? 0 : 1 / degree(g, x), intersect(neighbors(g, x), neighbors(g, y)))
                     scores[pair] = ra_score
                 end
             end
@@ -49,10 +50,10 @@ function get_CN_scores(g::SimpleGraph)::Dict{Tuple{Int,Int},Float64}
         neis = neighbors(g, u)
         for i in 1:length(neis)-1
             for j in i+1:length(neis)
-                v, w = neis[i], neis[j]
-                pair = (min(v, w), max(v, w))
+                x, y = neis[i], neis[j]
+                pair = (min(x, y), max(x, y))
                 if !haskey(scores, pair)
-                    cn_score = Float64(length(intersect(neighbors(g, v), neighbors(g, w))))
+                    cn_score = Float64(length(intersect(neighbors(g, x), neighbors(g, y))))
                     scores[pair] = cn_score
                 end
             end
@@ -61,7 +62,67 @@ function get_CN_scores(g::SimpleGraph)::Dict{Tuple{Int,Int},Float64}
     return scores
 end
 
+function get_RWR_scores(g::SimpleGraph; alpha=0.5)
+    eps = 1e-6
+    scores = Dict{Tuple{Int,Int},Float64}()
+    V = nv(g)
+    adj_mat = adjacency_matrix(g, Bool)
+    M_T = transpose(adj_mat ./ sum(adj_mat, dims=2))
+    p_vec::Vector{Vector{Float64}} = []
+    for v in vertices(g)
+        R = zeros(V)
+        R[v] = (1 - alpha)
+        p_curr::Vector{Float64} = zeros(V)
+        while true
+            p_t = alpha * (M_T * p_curr) + R
+            if sum((p_t .- p_curr) .^ 2) < eps
+                p_curr .= p_t
+                break
+            end
+            p_curr .= p_t
+        end
+        push!(p_vec, p_curr)
+    end
+    for x in vertices(g), y in vertices(g)
+        if x != y
+            pair = (min(x, y), max(x, y))
+            scores[pair] = p_vec[x][y] + p_vec[y][x]
+        end
+    end
+
+    return scores
+end
+
+function get_SRW_scores(g::SimpleGraph; lim=3)
+    scores = Dict{Tuple{Int,Int},Float64}()
+    V = nv(g)
+    adj_mat = adjacency_matrix(g, Bool)
+    M_T = transpose(adj_mat ./ sum(adj_mat, dims=2))
+    superposed_p_vec::Vector{Vector{Float64}} = []
+    for v in vertices(g)
+        p_curr::Vector{Float64} = zeros(V)
+        p_curr[v] = 1
+        superposition = zeros(V)
+        for _ in 1:lim
+            p_curr = M_T * p_curr
+            superposition .+= p_curr
+        end
+        push!(superposed_p_vec, superposition)
+    end
+
+    for x in vertices(g), y in vertices(g)
+        if x != y
+            pair = (min(x, y), max(x, y))
+            scores[pair] = (degree(g, x) * superposed_p_vec[x][y] + degree(g, y) * superposed_p_vec[y][x])
+        end
+    end
+
+    return scores
+end
+
 const score_type_dict = Dict(
     :cn => get_CN_scores,
-    :ra => get_RA_scores
+    :ra => get_RA_scores,
+    :rwr => get_RWR_scores,
+    :srw => get_SRW_scores
 )
